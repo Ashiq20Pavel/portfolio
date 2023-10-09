5336f7803c2fcfecb23669cb2330e65097960347
Md Ashiqur Rahman <ashiq.pavel@outlook.com>
2023-10-09 22:14:01 +0600

    CV added

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Md_Firoze_Hossain.pdf";
+import pdf from "../../Assets/../Assets/CV_Md_Ashiqur_Rahman.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/roze28/portfolio/master/src/Assets/Md_Firoze_Hossain.pdf";
+  "https://raw.githubusercontent.com/roze28/portfolio/master/src/Assets/CV_Md_Ashiqur_Rahman.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
